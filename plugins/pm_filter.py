c0df668c3ceec85061ad88d5d9c55a9c65c5062a
tbot6275 <98144208+tbot6275@users.noreply.github.com>
2022-01-24 19:00:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -64,7 +64,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"[{get_size(file.file_size)}] ⚡ {file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
